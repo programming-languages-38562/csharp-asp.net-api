11670e8fed1b76c5fa0d16daea4b242a3434a1d4
= <galagarjohnmark11@gmail.com>
2025-09-06 11:44:30 +0800

    feat: Implemented csharp-asp.net-api

---
+++ StudentWebApi/Services/StudentService.cs
@@ -0,0 +1,63 @@
+using StudentWebApi.Models;
+
+namespace StudentWebApi.Service
+{
+    public class StudentService : IStudentService
+    {
+        private static Dictionary<long, Student> students = new Dictionary<long, Student>();
+        private static long id = 1;
+
+        public Student AddStudent(Student student)
+        {
+            if (student.StudentId == 0)
+            {
+                student.StudentId = id++;
+            }
+
+            students.Add(student.StudentId, student);
+            return student;
+        }
+
+        public bool DeleteStudent(long id)
+        {
+            if (students.ContainsKey(id))
+            {
+                students.Remove(id);
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return students.Values.ToList();
+        }
+
+        public Student? GetStudentById(long id)
+        {
+            if (students.ContainsKey(id))
+            {
+                return students[id];
+            }
+
+            return null;
+        }
+
+        public Student? UpdateStudent(long id, Student student)
+        {
+            if (students.ContainsKey(id))
+            {
+                Student currStudent = students[id];
+
+                currStudent.Name = student.Name;
+
+                currStudent.Course = student.Course;
+
+                return currStudent;
+            }
+
+            return null;
+        }
+    }
+}
