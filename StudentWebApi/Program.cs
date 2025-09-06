157de838895ff16133a38e8671f48191ce458fcb
AlbertCJC <johnalbertcarbajal172@gmail.com>
2025-09-06 16:54:09 +0800

    feat: implented Student-api

---
+++ StudentWebApi/Program.cs
@@ -0,0 +1,25 @@
+using StudentWebApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<StudentService>();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.MapControllers();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+
+
+app.Run();
+
